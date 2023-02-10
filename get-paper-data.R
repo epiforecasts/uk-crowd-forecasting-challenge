3bd4bb7a0b8e7fba0628b3ac3cab0ed6a1443ed2
nikosbosse <nikosbosse@gmail.com>
2023-02-10 14:09:42 +0100

    update data including data revisions

--- get-paper-data.R
+++ get-paper-data.R
@@ -27,81 +27,126 @@ time_stop <- as.Date("2021-08-16")
 # bash fetch-data.sh
 
 
-# ------------------------------ Obtain data revisions ----------------------- #
-earliest_date <- NULL
-min_data <- as.Date("2021-03-08") ## earliest date to pot in data
-
-sources <-
-  c(Deaths = "JHU")
-
-target_variables <-
-  c(Deaths = "inc death")
-
-owner <- "epiforecasts"
-repo <- "covid19-forecast-hub-europe"
-path <- vapply(names(sources), function(x) {
-  paste("data-truth", sources[[x]],
-        paste0("truth_", sources[[x]], "-Incident ", x, ".csv"),
-        sep = "/")
-}, "")
-names(path) <- names(sources)
-
-data <- list()
-for (source in names(sources)) {
-  query <- "/repos/{owner}/{repo}/commits?path={path}"
-  if (!is.null(earliest_date)) {
-    query <- paste0(query, "&since={date}")
+# ---------------load truth data using the covidHubutils package ------------- #
+if (file.exists("data/weekly-truth-revised.csv")) {
+  truth_revised <- fread("data/weekly-truth-revised.csv")
+  daily_truth_revised <- fread("data/daily-truth-revised.csv")
+  
+} else {
+  clean_truth <- function(data) {
+    data |>
+      filter(target_variable %in% c("inc case", "inc death")) |>
+      mutate(target_variable = ifelse(target_variable == "inc case", 
+                                      "Cases", "Deaths")) |>
+      rename(target_type = target_variable, 
+             true_value = value) |>
+      select(-model) |>
+      filter(location == "GB") |>
+      filter(target_end_date >= "2021-01-01",
+             target_end_date <= "2022-01-01")
   }
   
-  commits <-
-    gh::gh(query,
-           owner = owner,
-           repo = repo,
-           path = path[source],
-           date = earliest_date,
-           .limit = Inf
+  truth_revised <- covidHubUtils::load_truth(hub = "ECDC") |>
+    clean_truth()
+  daily_truth_revised <- covidHubUtils::load_truth(hub = "ECDC", temporal_resolution = "daily") |>
+    clean_truth()
+  
+  truth_revised <- flag_anomalies(
+    mutate(truth_revised, target_end_date = as.character(target_end_date))
     )
   
-  shas <- vapply(commits, "[[", "", "sha")
-  dates <- vapply(commits, function(x) x[["commit"]][["author"]][["date"]], "")
-  dates <- as_date(ymd_hms(dates))
-  
-  ## keep multiples of 7 since today
-  select_commits <- which(as.integer(max(dates) - dates) %% 7 == 0)
-  
-  data[[source]] <-
-    lapply(
-      select_commits,
-      function(id)
-        readr::read_csv(
-          URLencode(
-            paste("https://raw.githubusercontent.com", owner, repo,
-                  shas[id], path[source], sep = "/")),
-          show_col_types = FALSE) %>%
-        mutate(commit_date = dates[id])
-    )
-  # remove empty dataframes
-  if (class(data[[source]]) == "list") {
-    data[[source]] <- data[[source]][sapply(data[[source]], function(x) nrow(x)>0)]
-  }
-  
-  data[[source]] <- data[[source]] %>%
-    bind_rows() %>%
-    mutate(type = {{ source }})
+  fwrite(truth_revised, "data/weekly-truth-revised.csv")
+  fwrite(daily_truth_revised, "data/daily-truth-revised.csv")
 }
 
-data_revisions <- data |>
-  bind_rows() |>
-  filter(location == "GB") |>
-  filter(date >= "2021-01-01", 
-         date <= "2022-01-01") |>
-  mutate(target_end_date = ceiling_date(date, "week", week_start = 6)) |>
-  group_by(location, location_name, target_end_date, commit_date, type) %>%
-  mutate(weekly_value = sum(value), 
-         n = n()) |>
-  ungroup() 
 
-fwrite(data_revisions, file = "data/data-revisions-deaths.csv")
+# ------------------------------ Obtain data revisions ----------------------- #
+if (file.exists("data/data-revisions-deaths.csv")) {
+  data_revisions <- fread("data/data-revisions-deaths.csv")
+} else {
+  earliest_date <- NULL
+  min_data <- as.Date("2021-03-08") ## earliest date to pot in data
+  
+  sources <-
+    c(Deaths = "JHU")
+  
+  target_variables <-
+    c(Deaths = "inc death")
+  
+  owner <- "epiforecasts"
+  repo <- "covid19-forecast-hub-europe"
+  path <- vapply(names(sources), function(x) {
+    paste("data-truth", sources[[x]],
+          paste0("truth_", sources[[x]], "-Incident ", x, ".csv"),
+          sep = "/")
+  }, "")
+  names(path) <- names(sources)
+  
+  data <- list()
+  for (source in names(sources)) {
+    query <- "/repos/{owner}/{repo}/commits?path={path}"
+    if (!is.null(earliest_date)) {
+      query <- paste0(query, "&since={date}")
+    }
+    
+    commits <-
+      gh::gh(query,
+             owner = owner,
+             repo = repo,
+             path = path[source],
+             date = earliest_date,
+             .limit = Inf
+      )
+    
+    shas <- vapply(commits, "[[", "", "sha")
+    dates <- vapply(commits, function(x) x[["commit"]][["author"]][["date"]], "")
+    dates <- as_date(ymd_hms(dates))
+    
+    ## keep multiples of 7 since today
+    select_commits <- which(as.integer(max(dates) - dates) %% 7 == 0)
+    
+    data[[source]] <-
+      lapply(
+        select_commits,
+        function(id)
+          readr::read_csv(
+            URLencode(
+              paste("https://raw.githubusercontent.com", owner, repo,
+                    shas[id], path[source], sep = "/")),
+            show_col_types = FALSE) %>%
+          mutate(commit_date = dates[id])
+      )
+    # remove empty dataframes
+    if (class(data[[source]]) == "list") {
+      data[[source]] <- data[[source]][sapply(data[[source]], function(x) nrow(x)>0)]
+    }
+    
+    data[[source]] <- data[[source]] %>%
+      bind_rows() %>%
+      mutate(type = {{ source }})
+  }
+  
+  data_revisions <- data |>
+    bind_rows() |>
+    filter(location == "GB") |>
+    filter(date >= "2021-01-01", 
+           date <= "2022-01-01") |>
+    mutate(target_end_date = ceiling_date(date, "week", week_start = 6)) |>
+    group_by(location, location_name, target_end_date, commit_date, type) %>%
+    mutate(weekly_value = sum(value), 
+           n = n()) |>
+    ungroup() 
+  
+  fwrite(data_revisions, file = "data/data-revisions-deaths.csv")
+}
+
+
+
+
+
+## -------------------------------------------------------------------------- ##
+# --------------------------------- Process data ----------------------------- #
+## -------------------------------------------------------------------------- ##
 
 # -------------------- Helper function to flag anomalies --------------------- #
 # function to filter out forecast anomalies based on the Forecast Hub anomalies file
@@ -157,45 +202,15 @@ clean_forecasts <- function(forecasts) {
     mutate(horizon = as.numeric(substring(target, 1, 1))) |>
     filter(type == "quantile") |>
     select(location, forecast_date, quantile, prediction, 
-                  horizon, model, target_end_date, target, target_type) |>
+           horizon, model, target_end_date, target, target_type) |>
     filter(forecast_date >= "2021-05-24", 
-                  forecast_date <= "2021-08-16") |>
+           forecast_date <= "2021-08-16") |>
     filter(model != "EpiNow2") |>
     select(-target)
 }
 
 
 
-
-# ---------------load truth data using the covidHubutils package ------------- #
-if (file.exists("data/weekly-truth.csv")) {
-  truth <- fread("data/weekly-truth.csv")
-  daily_truth <- fread("data/daily-truth.csv")
-  
-} else {
-  clean_truth <- function(data) {
-    data |>
-      filter(target_variable %in% c("inc case", "inc death")) |>
-      mutate(target_variable = ifelse(target_variable == "inc case", 
-                                      "Cases", "Deaths")) |>
-      rename(target_type = target_variable, 
-             true_value = value) |>
-      select(-model) |>
-      filter(location == "GB") |>
-      filter(target_end_date >= "2021-01-01",
-             target_end_date <= "2022-01-01")
-  }
-  
-  truth <- covidHubUtils::load_truth(hub = "ECDC") |>
-    clean_truth()
-  daily_truth <- covidHubUtils::load_truth(hub = "ECDC", temporal_resolution = "daily") |>
-    clean_truth()
-  
-  truth <- flag_anomalies(mutate(truth, target_end_date = as.character(target_end_date)))
-  
-  fwrite(truth, "data/weekly-truth.csv")
-  fwrite(daily_truth, "data/daily-truth.csv")
-}
 
 
 # --------- create version of the truth data without revisions --------------- #
@@ -204,8 +219,11 @@ daily_truth_original <- data_revisions |>
   select(location, target_end_date = date, 
          target_type = type, true_value = value) |>
   unique() |>
-  rbind(daily_truth |>
-          filter(target_type == "Cases"), fill = TRUE) |>
+  mutate(target_end_date = as.Date(target_end_date)) |>
+  rbind(daily_truth_revised |>
+          filter(target_type == "Cases") |>
+          mutate(target_end_date = as.Date(target_end_date)), 
+        fill = TRUE) |>
   arrange(target_end_date, target_type)
 
 fwrite(daily_truth_original, "data/daily-truth-original.csv")
@@ -217,7 +235,7 @@ weekly_truth_original <- data_revisions |>
          true_value = weekly_value) |>
   unique() |>
   arrange(target_end_date) |>
-  rbind(weekly_truth |>
+  rbind(weekly_truth_revised |>
           filter(target_type == "Cases"), 
         fill = TRUE) |>
   arrange(target_end_date, target_type) |>
@@ -230,13 +248,11 @@ fwrite(weekly_truth_original, "data/weekly-truth-original.csv")
 
 
 
-
-
 # ---------------------- load aggregated prediction data --------------------- #
 load_single_model <- function(root_dir,
                               model_name) {
   
-  locations <- select(truth, location, location_name) |>
+  locations <- select(weekly_truth_original, location, location_name) |>
     filter(location == "GB") |>
     unique()
   
@@ -277,63 +293,81 @@ load_forecasts <- function(directories,
   return(forecasts)
 }
 
-forecasts <- load_forecasts(
-  directories = c(
-    here("data", "submissions", "crowd-forecasts"), 
-    here("data", "submissions", "crowd-rt-forecasts"), 
-    here("data", "submissions", "crowd-direct-forecasts"), 
-    here("data", "submissions", "rt-forecasts")
-  ), 
-  model_names = c(
-    "crowd-ensemble", # is the same as the submitted EpiExpert-ensemble
-    "crowd-direct", 
-    "crowd-rt", 
-    "EpiNow2"
-  )
-) |>
-  clean_forecasts()
-
 # ---------------------- load individual prediction data --------------------- #
 
-root_dirs <- c(here::here("data", "crowd-direct-forecast", "processed-forecast-data"),
-               here::here("data", "crowd-rt-forecast", "processed-forecast-data"), 
-               here::here("data", "EuroCOVIDhub-ensemble", "processed-forecast-data"))
-file_paths_forecast <- c(here::here(root_dirs[1], list.files(root_dirs[1])),
-                         here::here(root_dirs[2], list.files(root_dirs[2])), 
-                         here::here(root_dirs[3], list.files(root_dirs[3])))
-
-individual_prediction_data <- purrr::map_dfr(file_paths_forecast,
-                                             .f = function(x) {
-                                               data <- data.table::fread(x)
-                                               if ("board_name" %in% names(data)) {
-                                                 setnames(data, old = "board_name", new = "model")
-                                               }
-                                               if (!("model" %in% names(data))) {
-                                                 data[, model := "EuroCOVIDhub-ensemble"]
-                                               }
-                                               data[, target_end_date := as.character(target_end_date)]
-                                               data[, forecast_date := calc_submission_due_date(forecast_date)]
-                                               data[, submission_date := as.character(forecast_date)]
-                                               if (grepl("-rt", x)) {
-                                                 data[, model := paste(model, "(Rt)")]
-                                               }
-                                               return(data)
-                                             }) |>
-  clean_forecasts()
-
-
-combined_forecasts <- rbind(forecasts, individual_prediction_data)
-
-data <- merge_pred_and_obs(combined_forecasts, 
-                               truth |>
-                                 mutate(target_end_date = as.character(target_end_date)) |>
-                                 filter(target_end_date >= "2021-01-01", 
-                                        target_end_date <= "2022-01-01"), 
-                               by = c("location", "target_end_date", 
-                                      "target_type")) |>
-  unique()
-
-fwrite(data, "data/forecast-data.csv")
+if (file.exists("data/forecast-data.csv")) {
+  data <- fread("data/forecast-data.csv")
+} else {
+  
+  forecasts <- load_forecasts(
+    directories = c(
+      here("data", "submissions", "crowd-forecasts"), 
+      here("data", "submissions", "crowd-rt-forecasts"), 
+      here("data", "submissions", "crowd-direct-forecasts"), 
+      here("data", "submissions", "rt-forecasts")
+    ), 
+    model_names = c(
+      "crowd-ensemble", # is the same as the submitted EpiExpert-ensemble
+      "crowd-rt", 
+      "crowd-direct", 
+      "EpiNow2"
+    )
+  ) |>
+    clean_forecasts()
+  
+  root_dirs <- c(here::here("data", "crowd-direct-forecast", "processed-forecast-data"),
+                 here::here("data", "crowd-rt-forecast", "processed-forecast-data"), 
+                 here::here("data", "EuroCOVIDhub-ensemble", "processed-forecast-data"))
+  file_paths_forecast <- c(here::here(root_dirs[1], list.files(root_dirs[1])),
+                           here::here(root_dirs[2], list.files(root_dirs[2])), 
+                           here::here(root_dirs[3], list.files(root_dirs[3])))
+  
+  individual_prediction_data <- purrr::map_dfr(file_paths_forecast,
+                                               .f = function(x) {
+                                                 data <- data.table::fread(x)
+                                                 if ("board_name" %in% names(data)) {
+                                                   setnames(data, old = "board_name", new = "model")
+                                                 }
+                                                 if (!("model" %in% names(data))) {
+                                                   data[, model := "EuroCOVIDhub-ensemble"]
+                                                 }
+                                                 data[, target_end_date := as.character(target_end_date)]
+                                                 data[, forecast_date := calc_submission_due_date(forecast_date)]
+                                                 data[, submission_date := as.character(forecast_date)]
+                                                 if (grepl("-rt", x)) {
+                                                   data[, model := paste(model, "(Rt)")]
+                                                 }
+                                                 return(data)
+                                               }) |>
+    clean_forecasts()
+  
+  
+  combined_forecasts <- rbind(forecasts, individual_prediction_data)
+  
+  data <- merge_pred_and_obs(combined_forecasts, 
+                             weekly_truth_original |>
+                               mutate(target_end_date = as.character(target_end_date)) |>
+                               filter(target_end_date >= "2021-01-01", 
+                                      target_end_date <= "2022-01-01"), 
+                             by = c("location", "target_end_date", 
+                                    "target_type")) |>
+    unique()
+  
+  fwrite(data, "data/forecast-data.csv")
+  
+  data_revised <- merge_pred_and_obs(combined_forecasts, 
+                                     weekly_truth_revised |>
+                                       mutate(target_end_date = as.character(target_end_date)) |>
+                                       filter(target_end_date >= "2021-01-01", 
+                                              target_end_date <= "2022-01-01"), 
+                                     by = c("location", "target_end_date", 
+                                            "target_type")) |>
+    unique()
+  
+  fwrite(data_revised, "data/forecast-data-revised.csv")
+  
+  
+}
 
 
 # ---- collect data on how many models were included in the hub-ensemble ----- #
@@ -373,26 +407,26 @@ fwrite(ensemble_inclusions, "data/ensemble-inclusions.csv")
 data <- select(data, -status)
 
 ## Score forecasts, adding versions for the log and sqrt transformations
-scores <- data |>
-  mutate(scale = "natural") |>
-  # add log data
-  rbind(data |>
-          mutate(
-            scale = "log", 
-            true_value = log(true_value + 1), 
-            prediction = log(pmax(prediction, 0) + 1)
-          )) |>
-  rbind(data |>
-          mutate(scale = "sqrt", 
-                 true_value = sqrt(true_value), 
-                 prediction = sqrt(prediction))) |>
-  score(metrics = c("interval_score")) |>
-  summarise_scores(by = c("model", "location",
-                          "target_end_date", "forecast_date",
-                          "horizon", "target_type", "scale"), 
-                   na.rm = FALSE)
+score_data <- function(data) {
+  scores <- data |>
+    mutate(scale = "natural") |>
+    # add log data
+    rbind(data |>
+            mutate(
+              scale = "log", 
+              true_value = log(true_value + 1), 
+              prediction = log(pmax(prediction, 0) + 1)
+            )) |>
+    score(metrics = c("interval_score", "coverage")) |>
+    add_coverage(by = c("model", "target_type", "horizon")) |>
+    select(-coverage_deviation)
+  
+  scores[, type_and_scale := paste0(target_type, " - ", scale)]
+  
+  return(scores[])
+}
 
-scores[, type_and_scale := paste0(target_type, " - ", scale)]
+
 
 # # add median forecast to scores, needed for some downstream analysis
 # add_median_forecast <- function(scores, hub_data) {
@@ -408,5 +442,10 @@ scores[, type_and_scale := paste0(target_type, " - ", scale)]
 # }
 # scores <- add_median_forecast(scores, hub_data)
 
+scores <- score_data(data)
 fwrite(scores, here("data", "all-scores-crowd-forecasts.csv"))
+
+
+scores_revised <- score_data(data_revised)
+fwrite(scores_revised, here("data", "all-scores-crowd-forecasts-revised.csv"))
 
